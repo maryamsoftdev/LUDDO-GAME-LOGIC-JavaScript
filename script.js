6342704cdc96fe9cec7a1f455f915a9b14aca82a
maryamsoftdev <nabeelahmaryaam@gmail.com>
2023-02-07 19:38:55 +0500

    completed

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ dice.addEventListener('click',function(){
     console.log('roll');
     let playerRoll = game.roll();
     let compRoll = game.roll();
+    let winner = game.checker(playerRoll,compRoll);
+    console.log(winner);
     
 })
- 
+//class logic in javascript
 function DiceGame(){
     this.roll = function(){
         this.result = Math.floor(Math.random()*6)+1;
         return this.result;
+    }
+    this.checker = function(roll1,roll2){
+      if(roll1>roll2){
+        return'player wins'
+      }else if(roll2>roll1){
+       return 'computer wins'
+      } 
+      else {
+        return'tie';
+      }
     }
 }
