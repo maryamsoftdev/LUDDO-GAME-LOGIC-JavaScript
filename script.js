aa2c11673f3594610c58c3dc7f6e54e3d23bef64
maryamsoftdev <nabeelahmaryaam@gmail.com>
2023-02-07 19:52:08 +0500

    completed

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ const game = new DiceGame();
 const dice = document.createElement('div');
 dice.textContent = "roll here";
 document.body.appendChild(dice);
- 
-dice.addEventListener('click',function(){
+dice.addEventListener('click', function () {
     console.log('roll');
     let playerRoll = game.roll();
     let compRoll = game.roll();
-    let winner = game.checker(playerRoll,compRoll);
+    let winner = game.checker(playerRoll, compRoll);
     console.log(winner);
-    
+    dice.innerHTML = `Player ${playerRoll} vs Computer ${compRoll} <br>${winner}`;
 })
-//class logic in javascript
-function DiceGame(){
-    this.roll = function(){
-        this.result = Math.floor(Math.random()*6)+1;
+ 
+function DiceGame() {
+    this.roll = function () {
+        this.result = Math.floor(Math.random() * 6) + 1;
         return this.result;
     }
-    this.checker = function(roll1,roll2){
-      if(roll1>roll2){
-        return'player wins'
-      }else if(roll2>roll1){
-       return 'computer wins'
-      } 
-      else {
-        return'tie';
-      }
+    this.checker = function (roll1, roll2) {
+        if (roll1 > roll2) {
+            return 'Player Wins'
+        }
+        else if (roll2 > roll1) {
+            return 'Computer Wins'
+        }
+        else {
+            return 'Tie';
+        }
     }
 }
